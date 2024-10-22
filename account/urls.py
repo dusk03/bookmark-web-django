ac8fea7e2087a78d6e7da7cdb660fe4aeab899e0
Dang_Thang <dinhthanghp03@gmail.com>
2024-10-22 11:42:01 +0700

    add user profile

--- account/urls.py
+++ account/urls.py
@@ -1,18 +1,19 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
     # path('login/', auth_views.LoginView.as_view(), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # path(
-    #     'password-change/', 
-    #     auth_views.PasswordChangeView.as_view(), 
+    #     'password-change/',
+    #     auth_views.PasswordChangeView.as_view(),
     #     name='password'
     # ),
     # path(
-    #     'password/done/', 
-    #     auth_views.PasswordChangeDoneView.as_view(), 
+    #     'password/done/',
+    #     auth_views.PasswordChangeDoneView.as_view(),
     #     name='password_change_done'
     # ),
     # path(
@@ -35,7 +36,8 @@ urlpatterns = [
     #     auth_views.PasswordResetCompleteView.as_view(),
     #     name='password_reset_complete'
     # )
-    path('', views.dashboard, name='dashboard'),
-    path('',include('django.contrib.auth.urls'))
-
+    path("", views.dashboard, name="dashboard"),
+    path("", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
+    path("edit/", views.edit, name="edit"),
 ]
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -30,6 +30,10 @@ ALLOWED_HOSTS = []
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+ 
 # Application definition
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 INSTALLED_APPS = [
