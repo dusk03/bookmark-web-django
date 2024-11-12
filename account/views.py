5a4a406d24c953231e70e69f6dcf7345af6338b5
Dang_Thang <dinhthanghp03@gmail.com>
2024-11-12 14:30:58 +0700

    add create tracking actions to views

--- account/views.py
+++ account/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.http import require_POST
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile, Contact
-
+from actions.utils import create_action
 
 def user_login(request):
     if request.method == "POST":
@@ -43,6 +43,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, "account/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
@@ -111,6 +112,7 @@ def user_follow(request):
                     user_from = request.user,
                     user_to=user
                 )
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(
                     user_from=request.user,
