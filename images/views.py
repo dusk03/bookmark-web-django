08353dea2fe6a23012057d485cd2b72dd9161296
Dang_Thang <dinhthanghp03@gmail.com>
2024-12-28 16:43:54 +0700

    update for counting image view

--- images/views.py
+++ images/views.py
@@ -11,6 +11,17 @@ from .models import Image
 from actions.utils import create_action
 # Create your views here.
 
+
+import redis
+from django.conf import settings
+
+# connect to redis
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB
+)
+
 @login_required
 def image_create(request):
     if request.method == 'POST':
@@ -38,10 +49,12 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # increment total image views by 1
+    total_views = r.incr(f'image:{image.id}:views')
     return render(
         request,
         'images/image/detail.html',
-        {'section': 'images', 'image': image}
+        {'section': 'images', 'image': image, 'total_views': total_views}
     )
 
 @login_required
