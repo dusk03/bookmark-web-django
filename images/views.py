6b10be45647d81361a75f953ddd0494d22e00425
Dang_Thang <dinhthanghp03@gmail.com>
2025-01-01 15:35:14 +0700

    add trending page

--- images/views.py
+++ images/views.py
@@ -120,5 +120,5 @@ def image_ranking(request):
     most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
     return render(request,
         'images/image/ranking.html',
-        {'section': 'images', 'most_viewed': most_viewed}
+        {'section': 'ranking', 'most_viewed': most_viewed}
     )
